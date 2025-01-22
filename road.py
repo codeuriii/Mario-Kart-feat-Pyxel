47dd08d656df0bd2eb23af4112dbf8fa10e019a1
whyxxh <rubenbeg0908@gmail.com>
2025-01-22 09:31:49 +0400

    idea for road

---
+++ road.py
@@ -0,0 +1,13 @@
+import pyxel as p
+
+""" 
+chaque map est représentée par une liste de listes, chque nombre represente, soit:
+- une route a 90° 
+- une route a 45°
+- une route a 0°
+- un virage a 90°
+- un virage a 45°
+- un virage a 180°
+
+qu'on peut orienter dans les 4 directions
+"""
