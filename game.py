d5aff3c84f53938d491d5035014f0a9063cf7d06
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2025-01-22 08:52:48 +0400

    Add player

--- game.py
+++ game.py
@@ -1,13 +1,21 @@
 import pyxel as p
+from player import Player
 
 class Game:
     def __init__(self):
         p.init(160, 120)
         p.run(self.update, self.draw)
+        self.player = Player()
 
     def update(self):
-        pass 
+        pass
 
     def draw(self):
         p.cls(0)
         p.rect(10, 10, 20, 20, 9)
+
+    def run(self):
+        p.run(self.update, self.draw)
+    
+    async def broadcast(self):
+        await self.player.receive_message()
