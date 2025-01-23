6f5e7f2fb502934373f3fae2ff138a6d006769d5
CodeurIII <nathan2.che974@gmail.com>
2025-01-23 19:25:09 +0400

    Try fix this room error + load images

--- game.py
+++ game.py
@@ -15,17 +15,17 @@ class Game:
     def draw(self):
         p.cls(0)
         self.player.car.draw_car()
-        # p.rect(10, 10, 20, 20, 9)
 
     def run(self):
         loop = asyncio.get_event_loop()
         loop.run_in_executor(None, self.start_pyxel)
 
     def start_pyxel(self):
-        p.init(160, 120)
+        p.init(160, 120, fps=60, title="Mario Kart 8.5")
+        p.load("images.pyxres")
+        p.mouse(True)
         p.run(self.update, self.draw)
 
-    
     async def receive_message(self):
         try:
             async for message in self.websocket:
@@ -41,13 +41,18 @@ class Game:
             self.create_player(message)
         elif message == "run":
             self.run()
+        elif message == "this room is full error":
+            print("This room is full.")
+            self.websocket.close()
 
 
     def create_player(self, message):
-        self.players.append({
-            "id": message.split("-")[0].split("/")[1],
-            "color": message.split("-")[1].split("/")[1]
-        })
+        player_id = message.split("-")[0].split("/")[1]
+        if not any(player["id"] == player_id for player in self.players):
+            self.players.append({
+                "id": player_id,
+                "color": message.split("-")[1].split("/")[1]
+            })
         print(self.players)
     
     async def send_id_request(self):
