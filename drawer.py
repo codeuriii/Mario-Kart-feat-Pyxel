61eee4803769b9cf7b12c16f000f8fce9a8d3c2e
CodeurIII <nathan2.che974@gmail.com>
2025-01-23 19:30:38 +0400

    reformate the code

--- drawer.py
+++ drawer.py
@@ -2,15 +2,11 @@ import pyxel as p
 
 class Drawer:
     def __init__(self):
-        self.blue_car = (0, 0, 16, 16)
-        self.red_car = (16, 0, 16, 16)
-        self.green_car = (32, 0, 16, 16)
-        self.yellow_car = (48, 0, 16, 16)
         self.cars = {
-            "blue": self.blue_car,
-            "red": self.red_car,
-            "green": self.green_car,
-            "yellow": self.yellow_car
+            "blue": (0, 0, 16, 16),
+            "red": (16, 0, 16, 16),
+            "green": (32, 0, 16, 16),
+            "yellow": (48, 0, 16, 16)
         }
     
     def draw_car(self, color, x, y):
