8a121349fd2134bc1d2a8a16184a565036f0d97c
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2025-01-23 08:23:53 +0400

    Handle message + player gestion + fix websocket

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ class Game:
     def __init__(self, websocket):
         self.websocket = websocket
         self.player = Player(self.websocket)
+        self.players = []
 
     def update(self):
         pass
@@ -15,8 +16,12 @@ class Game:
         p.cls(0)
         p.rect(10, 10, 20, 20, 9)
 
-    async def run(self):
+    def run(self):
         print("run function")
+        loop = asyncio.get_event_loop()
+        loop.run_in_executor(None, self.start_pyxel)
+
+    def start_pyxel(self):
         p.init(160, 120)
         p.run(self.update, self.draw)
 
@@ -28,11 +33,30 @@ class Game:
             async for message in self.websocket:
                 print(f"Received from server: {message}")
                 await self.player.handle_message(message)
+                await self.handle_message(message)
             print("after async for")
         except websockets.ConnectionClosed:
             print("Disconnected from server.")
 
         print("receive message function end")
+
+    async def handle_message(self, message):
+        print("handle message function")
+        if message.startswith("create_player"):
+            self.create_player(message)
+        elif message == "run":
+            self.run()
+
+        print("handle message function end")
+
+    def create_player(self, message):
+        print("create player function")
+        self.players.append({
+            "id": message.split("-")[0].split("/")[1],
+            "color": message.split("-")[1].split("/")[1]
+        })
+        print("create player function end")
+        print(self.players)
     
     async def send_id_request(self):
         print("send load to server")
