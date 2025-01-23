b17bde428cd8de068c42ecdf63cb55bb5db7c56b
CodeurIII <nathan2.che974@gmail.com>
2025-01-23 19:25:35 +0400

    create drawer + set coos

--- drawer.py
+++ drawer.py
@@ -1,4 +1,4 @@
-
+import pyxel as p
 
 class Drawer:
     def __init__(self):
@@ -6,4 +6,12 @@ class Drawer:
         self.red_car = (16, 0, 16, 16)
         self.green_car = (32, 0, 16, 16)
         self.yellow_car = (48, 0, 16, 16)
-        self.cars = [self.blue_car, self.red_car, self.green_car, self.yellow_car]
+        self.cars = {
+            "blue": self.blue_car,
+            "red": self.red_car,
+            "green": self.green_car,
+            "yellow": self.yellow_car
+        }
+    
+    def draw_car(self, color, x, y):
+        p.blt(x, y, 0, *self.cars[color], 0)
