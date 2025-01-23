6e4353937839e3daf07b20127631a87927fa290e
CodeurIII <nathan2.che974@gmail.com>
2025-01-23 19:28:39 +0400

    Add speed accel + speed turn + turn force

--- car.py
+++ car.py
@@ -5,29 +5,38 @@ class Car:
     def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.speed = 1
+        self.speed = 0
         self.angle = 270
         self.color = color
         self.drawer = Drawer()
+        self.speed_turn = 5
+        self.speed_max = 3
+        self.acceleration = 0.1
+        self.deceleration = 0.05
 
     def move(self):
         self.x += self.speed * p.cos(self.angle)
         self.y += self.speed * p.sin(self.angle)
+        self.speed_turn = max(1, 5 - self.speed * 0.1)
+        if self.speed > self.speed_max:
+            self.speed = self.speed_max
+        elif self.speed < -self.speed_max:
+            self.speed = -self.speed_max
         self.speed *= 0.99
 
     def keyboard_input(self):
-        if p.btnp(p.KEY_UP):
-            self.speed += 100
-        elif p.btnp(p.KEY_DOWN):
-            self.speed -= 100
-        if p.btnp(p.KEY_LEFT):
-            self.angle -= 10
-        if p.btnp(p.KEY_RIGHT):
-            self.angle += 10
+        if p.btn(p.KEY_UP):
+            self.speed += self.acceleration
+        elif p.btn(p.KEY_DOWN):
+            self.speed -= self.deceleration
+        if p.btn(p.KEY_LEFT):
+            self.angle -= self.speed_turn
+        if p.btn(p.KEY_RIGHT):
+            self.angle += self.speed_turn
 
     def update(self):
         self.move()
         self.keyboard_input()
 
     def draw_car(self):
-        p.blt(self.x, self.y, 0, 0, 0, 16, 16, p.COLOR_BLACK)
+        self.drawer.draw_car(self.color, self.x, self.y)
