b6d454e8fc2fdc1845e695f51f0f1508b831d645
whyxxh <rubenbeg0908@gmail.com>
2025-01-24 07:18:17 +0400

    added road map implementation

--- road.py
+++ road.py
@@ -1,13 +1,26 @@
 import pyxel as p
+from drawer import Drawer
 
-""" 
-chaque map est représentée par une liste de listes, chque nombre represente, soit:
-- une route a 90° 
-- une route a 45°
-- une route a 0°
-- un virage a 90°
-- un virage a 45°
-- un virage a 180°
+road_map =  [[-1 for i in range(10)] for j in range(7)]
 
-qu'on peut orienter dans les 4 directions
-"""
+road_map = [
+    [1, 2, 1, 1, 1, 3, 5, 1, 1, 1],  # Horizontal road with turns
+    [4, -1, -1, -1, -1, -1, 2, 5, -1, 1],  # Left turns and open spaces
+    [4, 1, 0, 1, 0, 3, 2, 1, 0, 1],  # Connecting the roads
+    [4, 1, 5, 1, 0, 3, 2, 5, 1, 1],  # Additional roads connecting
+    [4, 1, 1, 3, 5, 1, 1, 2, 1, 1],  # Making turns and joining paths
+    [4, 1, 1, 0, 0, 2, 1, 1, 0, 1],  # Final turns
+    [-1, 1, 3, 5, 1, 0, 1, 1, 1, -1],  # Ending the path (some open spaces)
+]
+
+class Road:
+    def __init__(self):
+        self.road_map = road_map
+        self.drawer = Drawer()
+
+    def draw_road(self):
+        for y in range(5):
+            for x in range(5):
+                tile = self.road_map[y][x]
+                if tile >= 0:
+                    self.drawer.draw_road_tile(x * 16, y * 16, tile)
