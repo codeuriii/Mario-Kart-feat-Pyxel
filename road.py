75ecebebf4fc5a39eb69779649483d43d31487c0
Nathan974-creator <113228775+Nathan974-creator@users.noreply.github.com>
2025-01-24 14:43:00 +0400

    Create track 1 + create roads class for autocompletion + update dimensions of drawer

--- road.py
+++ road.py
@@ -1,24 +1,31 @@
-import pyxel as p
 from drawer import Drawer
 
-road_map = [
-    [1, 2, 1, 1, 1, 3, 5, 1, 1, 1],  # Horizontal road with turns
-    [4, -1, -1, -1, -1, -1, 2, 5, -1, 1],  # Left turns and open spaces
-    [4, 1, 0, 1, 0, 3, 2, 1, 0, 1],  # Connecting the roads
-    [4, 1, 5, 1, 0, 3, 2, 5, 1, 1],  # Additional roads connecting
-    [4, 1, 1, 3, 5, 1, 1, 2, 1, 1],  # Making turns and joining paths
-    [4, 1, 1, 0, 0, 2, 1, 1, 0, 1],  # Final turns
-    [-1, 1, 3, 5, 1, 0, 1, 1, 1, -1],  # Ending the path (some open spaces)
-]
+class Roads:
+    empty = -1
+    vertical = 0
+    horizontal = 1
+    haut_gauche = 5
+    haut_droite = 4
+    bas_droite = 3
+    bas_gauche = 2
 
 class Road:
     def __init__(self):
-        self.road_map = road_map
         self.drawer = Drawer()
+        roads = Roads()
+        self.track_1 = [
+            [roads.empty, roads.bas_droite,  roads.horizontal, roads.horizontal,  roads.horizontal,  roads.horizontal, roads.horizontal, roads.bas_gauche, roads.empty],
+            [roads.empty, roads.vertical,    roads.empty,      roads.empty,       roads.empty,       roads.empty,      roads.empty,      roads.vertical,   roads.empty],
+            [roads.empty, roads.haut_droite, roads.horizontal, roads.horizontal,  roads.bas_gauche,  roads.empty,      roads.empty,      roads.vertical,   roads.empty],
+            [roads.empty, roads.empty,       roads.empty,      roads.empty,       roads.vertical,    roads.empty,      roads.empty,      roads.vertical,   roads.empty],
+            [roads.empty, roads.bas_droite,  roads.horizontal, roads.horizontal,  roads.haut_gauche, roads.empty,      roads.empty,      roads.vertical,   roads.empty],
+            [roads.empty, roads.vertical,    roads.empty,      roads.empty,       roads.empty,       roads.empty,      roads.empty,      roads.vertical,   roads.empty],
+            [roads.empty, roads.haut_droite, roads.horizontal, roads.horizontal,  roads.horizontal,  roads.horizontal, roads.horizontal, roads.haut_gauche, roads.empty]
+        ]
 
     def draw_road(self):
-        for y in range(5):
-            for x in range(5):
-                tile = self.road_map[y][x]
+        for y in range(7):
+            for x in range(9):
+                tile = self.track_1[y][x]
                 if tile >= 0:
                     self.drawer.draw_road_tile(x * 32, y * 32, tile)
