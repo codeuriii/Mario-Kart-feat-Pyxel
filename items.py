583929fddaa2acb3d4520086d5e8844fa73e2c83
whyxxh <rubenbeg0908@gmail.com>
2025-01-25 10:13:16 +0400

    items for nathan

---
+++ items.py
@@ -0,0 +1,15 @@
+
+class Items:
+    boule_de_feu = 0,
+    carapace_verte = 1
+    carapace_rouge = 2
+    carapace_bleue = 3
+    peau_de_banane = 4
+    fleur_de_feu = 5
+    bombe = 6
+    horn = 7
+    bullet_bill = 8
+
+class Item:
+    def __init__(self):
+        pass
