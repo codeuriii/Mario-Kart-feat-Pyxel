0e2296deb896945fc1a6d21b7ce133edf314c035
whyxxh <rubenbeg0908@gmail.com>
2025-01-29 08:04:21 +0400

    commit for pull

--- items.py
+++ items.py
@@ -23,23 +23,12 @@ class Item:
         self.speed = 5
 
     def update(self, player_x, player_y, player_angle):
-        self.check_item_use(player_x, player_y, player_angle)
-
         self.x += self.x_vel
         self.y += self.y_vel
 
         if self.x < 0 or self.x > p.width or self.y < 0 or self.y > p.height:
             return False
         return True
-
-
-    def check_item_use(self, player_x, player_y, player_angle):
-        if p.btnp(p.KEY_E) and int(self.id) > 0:
-            match int(self.id):
-                case Items.carapace_verte:
-                    self.x = player_x
-                    self.y = player_y
-                    self.launch_green_shell(player_angle)
 
     def draw(self):
         if int(self.id) != Items.none: 
